f93254098e21dbb248e522d75f8442f30251c597
Tiangeng Lu <58899759+tiangenglu@users.noreply.github.com>
2023-08-06 22:15:32 -0400

    h1b,h2b,lpr table 8-11 in yearbook,08/06/23

--- 08052023_h1b_employer.py
+++ 08052023_h1b_employer.py
@@ -6,6 +6,8 @@ Created on Sat Aug  5 00:31:41 2023
 @author: Tiangeng Lu
 
 Download the H-1b employer data hub files between FY09 and FY23
+Download the H-2b data
+Download LPR Yearbook Tables 8 to 11 Expanded: https://www.dhs.gov/immigration-statistics/readingroom/lpr/table_8_to_11_expanded
 """
 
 import requests
@@ -28,3 +30,25 @@ for i in range(len(all_links)):
 
 # Data were saved to local directories without cleaning.
 # Next steps include running basic summary statistics by FY and adding NAICS definition to the data
+
+####################### Collect the H-2b Data, online .csv data have to be saved to drive. 
+####################### Online tables can be cleaned upon download
+
+h2b_url = 'https://www.uscis.gov/tools/reports-and-studies/h-2b-employer-data-hub/h-2b-employer-data-hub-files'
+h2b_selector = Selector(text = requests.get(h2b_url).content)
+h2b_links = h2b_selector.xpath('//*[contains(@href, ".csv")]/@href').extract()
+h2b_names = ['h2b_20'+link.split('/')[-1].split('.')[0].split('%')[1][-2:] for link in h2b_links]
+if not os.path.exists('h2b'): os.makedirs('h2b')
+for i in range(len(h2b_links)):
+    request.urlretrieve(h2b_links[i], 'h2b/'+ h2b_names[i] + '.csv')
+    
+####################### Collect LPR Yearbook Tables 8 to 11 Expanded #######################
+####################### Three tabs in each EXCEL file. Summary Statistics only #############
+lpr_url = 'https://www.dhs.gov/immigration-statistics/readingroom/lpr/table_8_to_11_expanded' 
+lpr_selector = Selector(text = requests.get(lpr_url).content)
+lpr_links = lpr_selector.xpath('//*[contains(@href, ".xlsx")]/@href').extract()
+# Links are in different structures. The following string split is an ad-hoc solution.
+lpr_names = ['lpr_'+link.split('fy')[1].split('_')[0] for link in lpr_links]
+if not os.path.exists('lpr'): os.makedirs('lpr')
+for i in range(len(lpr_links)):
+    request.urlretrieve(lpr_links[i], 'lpr/'+lpr_names[i]+'.xlsx')
