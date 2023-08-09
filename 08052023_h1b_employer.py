4b0c6b16c20711c7cfe401dc2870a94aeee86a93
Tiangeng Lu <58899759+tiangenglu@users.noreply.github.com>
2023-08-09 12:23:06 -0400

    added lpr scraping,w/ h1b & h2b

--- 08052023_h1b_employer.py
+++ 08052023_h1b_employer.py
@@ -8,6 +8,7 @@ Created on Sat Aug  5 00:31:41 2023
 Download the H-1b employer data hub files between FY09 and FY23
 Download the H-2b data
 Download LPR Yearbook Tables 8 to 11 Expanded: https://www.dhs.gov/immigration-statistics/readingroom/lpr/table_8_to_11_expanded
+Download OIS LPRs by Citizenship and Major Classes of Admission: https://www.dhs.gov/immigration-statistics/readingroom/LPR/LPR-by-major-class-and-country
 """
 
 import requests
@@ -52,3 +53,19 @@ lpr_names = ['lpr_'+link.split('fy')[1].split('_')[0] for link in lpr_links]
 if not os.path.exists('lpr'): os.makedirs('lpr')
 for i in range(len(lpr_links)):
     request.urlretrieve(lpr_links[i], 'lpr/'+lpr_names[i]+'.xlsx')
+    
+####### OIS LPRs by Citizenship and Major Classes of Admission
+lpr_by_url = 'https://www.dhs.gov/immigration-statistics/readingroom/LPR/LPR-by-major-class-and-country'  
+lpr_by_selector = Selector(text = requests.get(lpr_by_url).content)
+lpr_by_links = lpr_by_selector.xpath('//*[contains(@href, ".xlsx")]/@href').extract()
+# 'Lawful Permanent Residents (LPRs) by Citizenship and Major Class of Admission: FY2005 - FY2021',
+# 'Derivative (Spouses and Children) Employment-Based LPRs by Citizenship and Major Class of Admission: FY2005 - FY2021'
+lpr_by_names = lpr_by_selector.xpath('//*[contains(@data-sort-value, "FY2005 - FY2021")]/@data-sort-value').extract()
+if not os.path.exists('lprs_cit_classes'): os.makedirs('lprs_cit_classes')
+file = open('lprs_cit_classes/readme.txt','w')
+for name in lpr_by_names:
+    file.write(name + "\n")
+file.close()    
+# use simplified names instead of extracting information from web
+for i in range(len(lpr_by_links)):
+    request.urlretrieve(lpr_by_links[i], 'lprs_cit_classes/' + 'lpr_classes' + str(i) + '.xlsx')  
